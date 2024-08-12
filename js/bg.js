febbbae82571729a7e0d94bda1e495f7fe2a47bd
silver <silver.js.dev@gmail.com>
2024-08-11 22:26:22 -0300

    game of life on webgl done

--- js/bg.js
+++ js/bg.js
@@ -26,11 +26,101 @@ const firstFrame = ()=>{
   mCtx.putImageData(bgImg, 0, 0);
 }
 
+//<-- Program configuration -->//
+
+const programSetup = ()=>{
+  const vertShaderSource = [
+    `attribute vec2 position;`,
+    `varying vec2 texCoords;`,
+    `int lifeNumber=0;`,
+    `void main(){`,
+    `  texCoords = (position+1.0)/2.0;`,
+    `  texCoords.y = 1.0-texCoords.y;`,
+    `  gl_Position = vec4(position,0,1.0);`,
+  `}`
+  ].join('\n');
+  const fragShaderSource = [
+    `precision highp float;`,
+    `varying vec2 texCoords;`,
+    `uniform sampler2D textureSampler;`,
+    
+    `float xSize = 1.0/${mCanvas.width}.0;`,
+    `float ySize = 1.0/${mCanvas.height}.0;`,
+    
+    `void main(){`,
+    `  vec4 color = texture2D(textureSampler,texCoords);`,
+    `  vec4 aColor = texture2D(textureSampler,texCoords+vec2(-xSize,ySize));`,
+    `  vec4 bColor = texture2D(textureSampler,texCoords+vec2(0.0,ySize));`,
+    `  vec4 cColor = texture2D(textureSampler,texCoords+vec2(xSize,ySize));`,
+    `  vec4 dColor = texture2D(textureSampler,texCoords+vec2(-xSize,0.0));`,
+    `  vec4 eColor = texture2D(textureSampler,texCoords+vec2(xSize,0.0));`,
+    `  vec4 fColor = texture2D(textureSampler,texCoords+vec2(-xSize,-ySize));`,
+    `  vec4 gColor = texture2D(textureSampler,texCoords+vec2(0.0,-ySize));`,
+    `  vec4 hColor = texture2D(textureSampler,texCoords+vec2(xSize,-ySize));`,
+    
+    //check neighbors
+    `  float neighbors = aColor.r + bColor.r + cColor.r + dColor.r + eColor.r + fColor.r + gColor.r + hColor.r;`,
+    `  if(color.r>0.5){`,
+    
+    //alive rules
+    `    if(neighbors<2.0 || neighbors>3.0){`,
+    `      color=vec4(0.0,0.0,0.0,1.0);`,
+    `    }`,
+    `  }else{`,
+    //dead rules
+    `    if(neighbors==3.0){`,
+    `      color=vec4(1.0,1.0,1.0,1.0);`,
+    `    }`,
+    `  }`,
+    /////////////////////////////
+    `  gl_FragColor = color;`,
+    `}`
+  ].join('\n');
+  const vertShader = gl.createShader(gl.VERTEX_SHADER);
+  const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+  
+  gl.shaderSource(vertShader,vertShaderSource);
+  gl.shaderSource(fragShader,fragShaderSource);
+  
+  gl.compileShader(vertShader);
+  gl.compileShader(fragShader);
+  
+  //<-- Creating program -->//
+  const program = gl.createProgram();
+  gl.attachShader(program,vertShader);
+  gl.attachShader(program,fragShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+  
+  //<-- Creating vertices -->//
+  const vertices = new Float32Array([
+    -1,-1,-1,1,1,1,
+    -1,-1,1,1,1,-1
+  ]);
+  
+  //< Buffering data -->//
+  const vertexBuffer = gl.createBuffer();
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  
+  const positionLocation = gl.getAttribLocation(program, "position");
+  
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionLocation);
+  
+  const texture = gl.createTexture();
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}
+
 //<-- Resize function -->//
 
 const resizeCanvas = ()=>{
   const winBitX = window.innerWidth.toString(2).length - 3;
-  const winBitY = window.innerHeight.toString(2).length - 2;
+  const winBitY = window.innerHeight.toString(2).length - 3;
   const newWidth = Math.pow(2, winBitX);
   const newHeight = Math.pow(2, winBitY);
   let changed = false;
@@ -47,118 +137,11 @@ const resizeCanvas = ()=>{
   if(changed){
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
     firstFrame();
+    programSetup();
   }
 }
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
-
-//////////////////////////// unoptimized //////////////////////
-//<-- Setting up webGL canvas -->//
-
-gl.clearColor(1.0,0.0,1.0,1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-
-//<-- Setting up shader code -->//
-
-const vertShaderSource=`
-attribute vec2 position;
-varying vec2 texCoords;
-int lifeNumber=0;
-void main(){
-texCoords = (position+1.0)/2.0;
-texCoords.y = 1.0-texCoords.y;
-gl_Position = vec4(position,0,1.0);
-}
-`;
-
-const fragShaderSource=`
-precision highp float;
-varying vec2 texCoords;
-uniform sampler2D textureSampler;
-
-float xSize = 1.0/${mCanvas.width}.0;
-float ySize = 1.0/${mCanvas.height}.0;
-
-void main(){
-vec4 color = texture2D(textureSampler,texCoords);
-vec4 aColor = texture2D(textureSampler,texCoords+vec2(-xSize,ySize));
-vec4 bColor = texture2D(textureSampler,texCoords+vec2(0.0,ySize));
-vec4 cColor = texture2D(textureSampler,texCoords+vec2(xSize,ySize));
-vec4 dColor = texture2D(textureSampler,texCoords+vec2(-xSize,0.0));
-vec4 eColor = texture2D(textureSampler,texCoords+vec2(xSize,0.0));
-vec4 fColor = texture2D(textureSampler,texCoords+vec2(-xSize,-ySize));
-vec4 gColor = texture2D(textureSampler,texCoords+vec2(0.0,-ySize));
-vec4 hColor = texture2D(textureSampler,texCoords+vec2(xSize,-ySize));
-
-//check neighbors
-
-float neighbors = aColor.r + bColor.r + cColor.r + dColor.r + eColor.r + fColor.r + gColor.r + hColor.r;
-
-if(color.r>0.5){
-//alive rules	
-if(neighbors<2.0 || neighbors>3.0){
-color=vec4(0.0,0.0,0.0,1.0);
-}
-}else{
-//dead rules
-if(neighbors==3.0){
-color=vec4(1.0,1.0,1.0,1.0);	
-}
-}
-/////////////////////////////
-
-gl_FragColor = color;
-}
-`;
-
-//<-- Creating the shaders -->//
-
-const vertShader = gl.createShader(gl.VERTEX_SHADER);
-const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-gl.shaderSource(vertShader,vertShaderSource);
-gl.shaderSource(fragShader,fragShaderSource);
-
-gl.compileShader(vertShader);
-gl.compileShader(fragShader);
-
-
-//<-- Creating program -->//
-
-const program = gl.createProgram();
-gl.attachShader(program,vertShader);
-gl.attachShader(program,fragShader);
-gl.linkProgram(program);
-
-gl.useProgram(program);
-
-
-//<-- Creating vertices -->//
-
-const vertices = new Float32Array([
-  -1,-1,-1,1,1,1,
-  -1,-1,1,1,1,-1
-]);
-
-
-//< Buffering data -->//
-
-const vertexBuffer = gl.createBuffer();
-
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-const positionLocation = gl.getAttribLocation(program, "position");
-
-gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(positionLocation);
-
-const texture = gl.createTexture();
-gl.activeTexture(gl.TEXTURE0);
-gl.bindTexture(gl.TEXTURE_2D, texture);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
 function nextFrame(){
 
